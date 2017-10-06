bc6061a3fd9b4a45501c2a2271d955cd292d3ec7
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-10-06 14:41:54 +0600

    First SOAP example

---
+++ SimpleSOAPExample/src/org/arpit/javapostsforlearning/webservices/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.arpit.javapostsforlearning.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService()
+public class HelloWorld {
+    @WebMethod
+    public String sayHelloWorld(String name){
+        return "Привет "+name;
+    }
+    @WebMethod
+    public int plusTwoNumber(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Object implementor = new HelloWorld();
+        String address="http://localhost:9000/HelloWorld";
+        Endpoint.publish(address,implementor);
+    }
+}
